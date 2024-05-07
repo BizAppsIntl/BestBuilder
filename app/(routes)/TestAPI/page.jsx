c18e1609ab86a3a560daff026365c23c8b1b142d
BizAppsIntl <BizAppsIntl@gmail.com>
2024-05-07 14:06:00 +0500

    update

--- app/(routes)/TestAPI/page.jsx
+++ app/(routes)/TestAPI/page.jsx
@@ -1,6 +1,7 @@
+
 export default async function Page(){
   let data =await fetch (process.env.NEXT_PUBLIC_API_URL+'TestAPI')
   let json =await data.json()
   
-  return <h1>{JSON.stringify(json)}</h1>
+  return (JSON.stringify(json))
 }
